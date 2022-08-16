c949cf35a22123e1ca5665ff47f0c9eeb89239bc
sed177 <anzorovsed@gmail.com>
2022-08-16 19:03:11 +0300

    s

---
+++ index.js
@@ -0,0 +1,54 @@
+// console.log("Hello, World!")
+// console.log(2+3)
+// console.log(10-2);
+// console.log(2*3);
+// console.log(10/5);
+// console.log(2**3);
+// console.log((2+3)+(10-2)+(2*3)+(10/5)+(2**3));
+
+// let, const (переменные)
+// let n1 = 2+2
+// let n2 = 2*4
+// let n3 = 10/2
+// let n4 = 4-1
+// let n5 = 2**3
+// console.log(n1 + n2 + n3 + n4 + n5);
+// n1 = 10+2
+// console.log(n1 + n2 + n3 + n4 + n5)
+// типы данных
+// let num = 1 Number
+
+// let str1 = "My name is" String  
+// let str2 = 'my name is'
+// let str3 = `my name is`
+
+// let bool1 = true Boolean
+// let bool2 = false
+// let n1 = 5*2
+// let n2 = 17*8
+// let n3 = 66*3
+// let n4 = 43
+// let n5 = 45/4
+// let str1 = "Ibra"
+// let str2 = "Movs"
+// let str3 = "Sed"
+// let str4 = "Adam"
+// let str5 = "Vitalya"
+// console.log(n1, n2, n3, n4, n5, str1, str2, str3, str4, str5);
+// str1 = "Vanya"
+// str2 = "Kachok"
+// str3 = "John"
+// n1 = 145
+// n2 = 23
+// n3 = 301*2
+// console.log(n1, n2, n3, n4, n5, str1, str2, str3, str4, str5)
+
+// let nums = [1,2,3,4,5] массив чисел в квадратных скобках
+// let names = [Adam...] массив имен                              console.log(nums[0])
+
+let n1 = ["Movsar", 32, "Sed", 30, "Ibra", 25, "Movs", 24, "Medina", 1 ]
+console.log("Имя:", n1[2], ",", "Возраст", n1[3]);
+console.log("Имя:", n1[0], ",", "Возраст", n1[1])
+console.log("Имя:", n1[4], ",", "Возраст", n1[5])
+console.log("Имя:", n1[6], ",", "Возраст", n1[7]
+)
