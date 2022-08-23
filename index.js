72e2ab09239c52b8b58cd654d2c0e5849444cd5f
sed177 <anzorovsed@gmail.com>
2022-08-23 18:50:14 +0300

    cikl

--- index.js
+++ index.js
@@ -46,9 +46,153 @@
 // let nums = [1,2,3,4,5] массив чисел в квадратных скобках
 // let names = [Adam...] массив имен                              console.log(nums[0])
 
-let n1 = ["Movsar", 32, "Sed", 30, "Ibra", 25, "Movs", 24, "Medina", 1 ]
-console.log("Имя:", n1[2], ",", "Возраст", n1[3]);
-console.log("Имя:", n1[0], ",", "Возраст", n1[1])
-console.log("Имя:", n1[4], ",", "Возраст", n1[5])
-console.log("Имя:", n1[6], ",", "Возраст", n1[7]
-)
+// let n1 = ["Movsar", 32, "Sed", 30, "Ibra", 25, "Movs", 24, "Medina", 1]
+// console.log("Имя:", n1[2], ",", "Возраст", n1[3]);
+// console.log("Имя:", n1[0], ",", "Возраст", n1[1])
+// console.log("Имя:", n1[4], ",", "Возраст", n1[5])
+// console.log("Имя:", n1[6], ",", "Возраст", n1[7])
+
+
+                        //   20.08.2022
+// let myName = "Sed"
+// let age = 30
+// let myFriendsName = "James"
+// let car = "Aston martin db9"
+// let myCar = car
+// console.log("Моя машина " + myCar + " такая же как у " + myFriendsName + ". Его машина", car)
+// node index.js
+// let names = [
+//     ["Sed", 30],
+//     ["Anzor", 34],
+//     ["Shamil", 32],
+//     ["Ibra", 25],
+//     ["Adam", 22]
+// ]
+// console.log("My name is " + names[0][0] + ". I'am " + names[0][1] + ".");
+// console.log("My brother name is " + names[1][0] + ". Hi is " + names[1][1] + ".");
+// console.log("My brother name is " + names[2][0] + ". Hi is " + names[2][1] + ".");
+// console.log("My brother name is " + names[3][0] + ". Hi is " + names[3][1] + ".");
+// console.log("My brother name is " + names[4][0] + ". Hi is " + names[4][1] + ".");
+// let str = "I live in Grozny"
+// console.log(str [0]);
+// console.log(str [1]);
+// console.log(str [2]);
+// console.log(str [3]);
+// console.log(str [4]);
+// console.log(str [5]);
+// console.log(str [6]);
+// console.log(str [7]);
+// console.log(str [8]);
+// console.log(str [9]);
+// console.log(str [10]);
+// console.log(str [11]);
+// console.log(str [12]);
+// console.log(str [13]);
+// console.log(str [14]);
+// console.log(str [15]);
+
+// let index = 0
+// let religion = "ISLAM"
+// console.log(religion [index]);
+// console.log(religion [0 + 1]);
+// console.log(religion [ 0 + 2]);
+// console.log(religion [0 + 3]);
+// console.log(religion [0 + 4]);
+// console.log(religion [0 + 5]);
+
+// let in1 = 0
+// let in2 = 1
+// console.log(in1++);
+// console.log(++in2);
+
+// === сравнение
+// !== не равно       <   >   >=  <=
+
+// let str = "My name is Sed. I was born in Grozny."
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// console.log(str.length);
+// console.log(arr.length);
+
+// console.log(3<5);
+// console.log(3>5);
+// console.log(3<=3);
+// console.log("ab"==="ab");
+// console.log("ad"!=="ab");
+// console.log("ad"==="ad");
+
+// в условие иф заходим только когда true
+// если сработает одно из условий остальные работать не будут
+
+// let age = 10
+// let myName = "Luka"
+// let zh1 = "ужас"
+// let zh2 = "боевик"
+// let zh3 = "мультфильм"
+// if (age > 18) {
+//     console.log ("Вам "+ age+ " лет, поэтому можно смотреть "+ zh1+" жанр");
+// }
+// else if (age > 18) {
+//     console.log("Вам "+ age+ " лет, поэтому можно смотреть "+ zh2+" жанр");
+// }
+// else if (age > 6) {
+//     console.log("Вам "+ age+ " лет, поэтому можно смотреть жанр "+ zh3);
+// }
+
+// let str = "Chechnya"
+// console.log(str[0]);
+// console.log(str[1]);
+// console.log(str[2]);
+// console.log(str[3]);
+// console.log(str[4]);
+// console.log(str[5]);
+// console.log(str[6]);
+// console.log(str[7]);
+
+// let s = "Chechnya"
+// let i = 0
+
+// for (let i = 0; i <s.length; i++) {
+//     console.log(s[i]);
+// }
+// j=j+1   правильно писать j+=1
+
+// написать цикл от 21 до 122, цикл должен идти по нечетным 
+
+// for (let i = 21; i <122; i+=2) {
+//     console.log(i);
+// }
+// for (let i =2; i <37; i++) {if (i %2 === 0)
+//     console.log(i);
+// }
+// for (let i =2; i <37; i++) {if (i %2 === 1)
+//     console.log(i);
+// }
+// for (let i=0; i<=100; i++) {
+//     if (i% 2 === 0) {
+//         console.log("chetnoe");
+//     } else {i%2 === 1 }
+//     console.log("nechet");
+    
+// }
+// ||- или &&- и
+
+// let names = ["Adam", "Lee", "Arsen"]
+// for (i= 0; i< names.length; i++) {
+//     if (names [i]=== "Sed")
+//     console.log("I won");
+   
+// }
+// if (i=== names.length) {
+//     if (names[i] !== "Sed")
+//         console.log("I'am not on the list");
+// }
+// push добавить в конец массив; pop - удаляет из конца массива; добавить в начало массива - unshift; удаляет из конца массива - shift
+
+let names = ["Adam", "Lee", "Movsar", "Kazbek", "Alibek"]
+for (i= 0; i< names.length; i++) {
+    let currentName = names[i]
+    let firstLetter= currentName[0]
+    let lastLetter= currentName[currentName.length-1]
+    let nameLength= currentName.length
+    console.log(currentName, firstLetter, lastLetter, nameLength);
+}
